b69ce100e34a2c535f68d7ca39d319c3d0b062a9
asifk <19btcs001hy@manuu.edu.in>
2023-05-13 22:58:09 +0530

    done

--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -1,30 +1,73 @@
 package com.driver;
 
-import io.swagger.models.auth.In;
+
+
+import java.util.Arrays;
+import java.util.List;
+
 
 public class Order {
 
+
     private String id;
     private int deliveryTime;
+
+    public Order(String id, int deliveryTime) {
+        this.id = id;
+        this.deliveryTime = deliveryTime;
+    }
 
     public Order(String id, String deliveryTime) {
 
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
+        this.deliveryTime = covertDeliveryTime(deliveryTime);
+        this.id = id;
+    }
+    public Order() {
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
 
-        this.id=id;
+    public void setDeliveryTime(int deliveryTime) {
+        this.deliveryTime = deliveryTime;
+    }
 
-        String arr[]=deliveryTime.split(":");//12:45
-        int hr=Integer.parseInt(arr[0]);
-        int min=Integer.parseInt(arr[1]);
+    public void setDeliveryTime(String deliveryTime) {
 
-        this.deliveryTime=(hr*60+min);
+        this.deliveryTime = covertDeliveryTime(deliveryTime);
+    }
 
+    public static int covertDeliveryTime(String deliveryTime) {
+        List<String> list = Arrays.asList(deliveryTime.split(":"));
+        int HH = Integer.parseInt(list.get(0));
+        int MM = Integer.parseInt(list.get(1));
+        return HH * 60 + MM;
     }
 
     public String getId() {
         return id;
     }
 
-    public int getDeliveryTime() {return deliveryTime;}
+    public int getDeliveryTime() {
+        return deliveryTime;
+    }
+
+
+
+    public static String convertDeliveryTime(int deliveryTime) {
+        int HH = deliveryTime / 60;
+        int MM = deliveryTime % 60;
+        String hh = String.valueOf(HH);
+        String mm = String.valueOf(MM);
+        if (hh.length() == 1) {
+            hh = '0' + hh;
+        }
+        if (mm.length() == 1) {
+            mm = '0' + mm;
+        }
+        return hh + ":" + mm;
+    }
+
 }
